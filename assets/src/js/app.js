759514450bc07e429ea1076c0bb6804995bc391e
travis-ci <mail@torounit.com>
2015-12-23 14:07:35 +0000

    Update from travis

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,27 +1,21 @@
 var smoothScroll = require( '../../src/js/smooth-scroll.min.js' );
+
 jQuery( function( $ ) {
-
-	( function() {
-		"use strict";
-		$('.js-overlay-controller').on( 'click', function(){
-			$('.js-overlay').fadeToggle( 200 );
-		});
-	} )();
-
+	
 	/**
 	 * scroll
 	 */
 	( function() {
 		smoothScroll.init();
 	} )();
-
+	
 	/**
 	 * pagetop
 	 */
 	( function() {
 		var pagetop = $( '.pagetop' );
 		var scroll  = 0;
-
+		
 		function set_fixed_top() {
 			if ( scroll >= $( document ).height() - $( window ).height() ) {
 				var top = $( window ).height() - pagetop.height() - 90;
@@ -35,12 +29,12 @@ jQuery( function( $ ) {
 				pagetop.css( 'top',  top );
 			}
 		}
-
+			
 		$( window ).load( function() {
 			set_fixed_top();
 			pagetop.addClass( 'fade-in' );
 		} );
-
+		
 		var timer = false;
 		var start = 0;
 		$( window ).scroll( function() {
@@ -71,7 +65,7 @@ jQuery( function( $ ) {
 				}
 			}, 200 );
 		} );
-
+		
 		$( window ).scroll( function() {
 			scroll = $( window ).scrollTop();
 			var balloon = $( '.wapuu-pagetop__balloon' );
@@ -82,20 +76,20 @@ jQuery( function( $ ) {
 			}
 		} );
 	} )();
-
+	
 	/**
 	 * スマホフッター余白
 	 */
 	( function() {
 		set_sp_footer_margin();
-
+		
 		$( window ).resize( function() {
 			set_sp_footer_margin();
 		} );
-
+		
 		function set_sp_footer_margin() {
 			var gnav = $( '.global-nav' );
-
+			
 			if ( gnav.css( 'position' ) == 'fixed' ) {
 				var margin = gnav.height()
 			} else {
@@ -104,24 +98,24 @@ jQuery( function( $ ) {
 			$( 'body' ).css( 'padding-bottom', margin );
 		}
 	} )();
-
+	
 	/**
 	 * タイムラインの各セッションを上下くっつける
 	 */
 	( function() {
 		set_section_position();
-
+		
 		$( window ).resize( function() {
 			set_section_position();
 		} );
-
+		
 		function set_section_position() {
 			var item = $( '.timeline__item' );
 			if ( item.css( 'float' ) == 'none' ) {
 				item.css( 'margin-top', '' );
 				return;
 			}
-
+			
 			item.each( function( i, e ) {
 				if ( $( e ).hasClass( 'timeline__item--left' ) ) {
 					var brother = $( e ).prev( '.timeline__item--left' );
@@ -134,16 +128,16 @@ jQuery( function( $ ) {
 						var brother = $( e ).prev().prev( '.timeline__item--right' );
 					}
 				}
-
+				
 				if ( brother.length ) {
 					var item_position    = $( e ).position();
 					var brother_position = brother.position();
-
+					
 					if ( typeof item_position !== 'undefined' && typeof brother_position !== 'undefined' ) {
 						var item_top    = $( e ).position().top;
 						var brother_top = brother.position().top;
 						var brother_margin = parseInt( brother.css( 'margin-top' ) );
-
+						
 						$( e ).css( 'margin-top', brother_top - item_top + brother.outerHeight() + brother_margin  );
 					}
 				}
